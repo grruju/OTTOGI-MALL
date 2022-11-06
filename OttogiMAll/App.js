3c6a888a55e262953be487c5f1ca778afdca8e2f
grruju <89845801+grruju@users.noreply.github.com>
2022-11-06 23:26:08 +0900

    OTTOGI-MALL WebView
    2018-08-26 진행한 프로젝트

---
+++ OttogiMAll/App.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+export default class App extends React.Component {
+  render() {
+    return <WebView source={{ uri: 'https://www.ottogimall.co.kr/' }} style={{ marginTop: 20 }} />;
+  }
+}
